2cb277d98592c3bb6ddf249f4d86c25d1bfbaa1c
Ronaldo Cerenza <rockronald@gmail.com>
2023-01-24 18:27:36 -0400

    exercicios propostos feitos

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javaScript-Array-e-loop-For/ExerciciosPratica.js
@@ -0,0 +1,61 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let media =0;
+
+// Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+for (let index =0; index < numbers.length; index +=1) {
+  console.log(numbers[index]);
+}
+
+// Some todos os valores contidos no array e imprima o resultado;
+for (let index =0; index < numbers.length; index +=1) {
+  soma = soma + (numbers[index]);
+}
+console.log(soma);
+
+// Calcule e imprima a média aritmética dos valores contidos no array;
+// Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+media = soma / numbers.length
+console.log(media);
+
+// Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
+
+if (media > 20) {
+  console.log('valor maior que 20')
+} else {
+  console.log('valor menor ou igual a 20');
+}
+
+// Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+let maiorNumero = numbers[0];
+
+for (let index =1; index < numbers.length; index +=1) {
+if (numbers[index] > maiorNumero) {
+  maiorNumero = (numbers[index]);
+}
+}
+console.log(maiorNumero);
+
+// Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
+let impares = [];
+
+for (let index =0; index < numbers.length; index +=1) {
+  if (numbers[index]%2!==0){
+    impares.push(numbers[index]);
+  } 
+  };
+  
+  console.log(impares)
+
+  // Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+  
+let menorNumero = numbers[0];
+
+for (let index =1; index < numbers.length; index +=1) {
+if (numbers[index] < menorNumero) {
+  menorNumero = (numbers[index]);
+}
+}
+console.log(menorNumero);
+
---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javaScript-Array-e-loop-For/diaDois01.js
@@ -0,0 +1,23 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0;index < groceryList.length; index+= 1){
+  console.log(groceryList[index]);
+}
+
+// Para fixar
+// Utilize o for/of para imprimir os elementos da lista names com o console.log():
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (Name of names){
+  console.log(Name);
+}
+
+let money = ['Real', 'Dolar', 'Euro', 'Libra'];
+
+for (money of money){
+  console.log(money);
+}
+
+
+
