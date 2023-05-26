77912241b7b487f7fc032265177477388d28c616
Ronaldo Cerenza <rockronald@gmail.com>
2023-05-26 17:26:57 -0400

    hook userState

---
+++ front-end/hooks-exercise/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Forms from "./components/Forms";
+
+function App() {
+  return (
+    <div className="bg-green-500">
+      <Forms />
+    </div>
+  );
+}
+
+export default App;
