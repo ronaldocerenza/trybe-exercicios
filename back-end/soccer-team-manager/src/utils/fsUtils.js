1e5ecb1d41589640fc59ec3d1fa5a514748c5b19
Ronaldo Cerenza <rockronald@gmail.com>
2023-07-19 12:13:41 -0400

    refatorando e retirando uma função number()

--- back-end/soccer-team-manager/src/utils/fsUtils.js
+++ back-end/soccer-team-manager/src/utils/fsUtils.js
@@ -24,24 +24,69 @@ async function writeSoccerTeams (newTeam) {
 // async é uma função assíncrona, que vai escrever no arquivo
 try {
   const oldTeams = await readSoccerTeams();
-  const allTeams = JSON.stringify([
-    ...oldTeams,
-    {id: Date.now(),
-    ...newTeam}]);
+  const newTeamWithId = { id: Date.now(), ...newTeam };
+  const allTeams = JSON.stringify([...oldTeams, newTeamWithId]);
   // oldTeams recebe o readSoccerTeams, que lê o arquivo
   // allTeams recebe o oldTeams, que é o arquivo lido, e o newTeam, que é o novo time
   await fs.writeFile(path.resolve(__dirname,TEAMS_FILE_PATH), allTeams);
   // o fs vai escrever no arquivo, e o path vai ler o caminho
   // o JSON vai transformar o objeto em um arquivo
+  return newTeamWithId;
 } catch (error) {
   console.error(`Erro ao escrever no arquivo: ${error}`);
   // se der erro, retorna uma mensagem de erro
 }
 };
 
+//OBJETIVO: atualizar um arquivo para escrever no json
+async function updateSoccerTeams (id, updatedTeamData) {
+// async é uma função assíncrona, que vai atualizar o arquivo
+  const oldTeams = await readSoccerTeams();
+  const updatedTeam = { id, ...updatedTeamData };
+  const updatedTeams = oldTeams.reduce((teamsList, currentTeams) => {
+    if (currentTeams.id === updatedTeam.id) return [...teamsList, updatedTeam];
+    return [...teamsList, currentTeams];
+  }, []);
+  // oldTeams recebe o readSoccerTeams, que lê o arquivo
+  // updatedTeam recebe o id e o updatedTeamData
+  // updatedTeams recebe o oldTeams, que é o arquivo lido, e o updatedTeam, que é o time atualizado
+
+  const updatedTeamsJSON = JSON.stringify(updatedTeams);
+  try {
+    await fs.writeFile(path.resolve(__dirname,TEAMS_FILE_PATH), updatedTeamsJSON);
+    console.log(`Time atualizado com id ${id}!`);
+  // o fs vai escrever no arquivo, e o path vai ler o caminho
+  // o JSON vai transformar o objeto em um arquivo
+    return updatedTeam;
+  } catch (error) {
+    console.error(`Erro ao escrever no arquivo: ${error}`);
+  }
+};
+
+// OBJETIVO: deletar um time do arquivo
+async function deleteSoccerTeams (id) {
+// async é uma função assíncrona, que vai deletar o arquivo
+  const oldTeams = await readSoccerTeams();
+  const updatedTeams = oldTeams.filter((team) => team.id !== Number(id));
+  // oldTeams recebe o readSoccerTeams, que lê o arquivo
+  // updatedTeams recebe o oldTeams, que é o arquivo lido, e o id, que é o id do time
+
+  const updatedTeamsJSON = JSON.stringify(updatedTeams);
+  try {
+    await fs.writeFile(path.resolve(__dirname,TEAMS_FILE_PATH), updatedTeamsJSON);
+    console.log(`Time atualizado com id ${id}!`);
+  // o fs vai escrever no arquivo, e o path vai ler o caminho
+  // o JSON vai transformar o objeto em um arquivo
+  } catch (error) {
+    console.error(`Erro ao escrever no arquivo: ${error}`);
+  }
+};
+
 module.exports = {
   readSoccerTeams,
-  writeSoccerTeams
+  writeSoccerTeams,
+  updateSoccerTeams,
+  deleteSoccerTeams,
 };
 // exportando o readSoccerTeams para ser utilizado em outros arquivos
 // exportando o writeSoccerTeams para ser utilizado em outros arquivos
